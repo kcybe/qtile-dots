498c309cbaf8c59152ab8d0284d672d4156f3c59
kcybe <75665158+kcybe@users.noreply.github.com>
2021-07-11 16:01:17 +0300

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -7,4 +7,7 @@ xrandr --output HDMI-0 --mode 3440x1440 --rate 100 &
 feh --bg-fill Pictures/Wallpapers/wallhaven-lmxrrp.png &
 
 # Picom Settings
-picom -b --experimental-backends # Starting Picom
+picom -b --experimental-backends & # Starting Picom 
+
+# Mouse Settings
+xinput set-prop "Logitech Gaming Mouse G502" "Coordinate Transformation Matrix" 2.000000, 0.000000, 0.000000, 0.000000, 2.000000, 0.000000, 0.000000, 0.000000, 1.000000 # Sensitivity
